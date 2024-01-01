955749c3c7ddc0d993c2e7e35fca6dd2a607d66d
bizboz1981 <willjsaunders@icloud.com>
2024-01-01 17:05:03 +0000

    added background img

--- logic.js
+++ logic.js
@@ -4,11 +4,21 @@ const heading = document.querySelector('h1');
 // Add an event listener for the 'mouseover' event
 heading.addEventListener('mouseover', () => {
     // Change the color of the <h1> element on hover
-    heading.style.color = 'red';
+    heading.style.color = 'teal';
 });
 
 // Add an event listener for the 'mouseout' event
 heading.addEventListener('mouseout', () => {
     // Reset the color of the <h1> element when the mouse is no longer hovering over it
     heading.style.color = '';
+});
+
+const tsbb = document.getElementById('tsbb');
+
+tsbb.addEventListener('mousedown', () => {
+    tsbb.style.color = 'red';
+});
+
+tsbb.addEventListener('mouseup', () => {    
+    tsbb.style.color = '';
 });
